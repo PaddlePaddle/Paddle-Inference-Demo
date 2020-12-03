e252cad453402b228080e7c6a04296c63ce415a2
jiweibo <jiweibo@baidu.com>
2020-12-03 10:55:26 +0000

    update mask demo with 2.0 api

--- python/mask_detection/models/pd_model.py
+++ python/mask_detection/models/pd_model.py
@@ -1,39 +1,42 @@
 import numpy as np
-from paddle.fluid.core import AnalysisConfig
-from paddle.fluid.core import create_paddle_predictor
+from paddle.inference import Config
+from paddle.inference import create_predictor
+
 
 class Model:
-   def __init__(self, model_file, params_file, use_mkldnn=True, use_gpu = False, device_id = 0):
-     config = AnalysisConfig(model_file, params_file)
-     config.switch_use_feed_fetch_ops(False)
-     config.switch_specify_input_names(True)
-     config.enable_memory_optim()
+    def __init__(self,
+                 model_file,
+                 params_file,
+                 use_mkldnn=True,
+                 use_gpu=False,
+                 device_id=0):
+        config = Config(model_file, params_file)
+        config.enable_memory_optim()
 
-     if use_gpu:
-       print ("ENABLE_GPU")
-       config.enable_use_gpu(100, device_id)
+        if use_gpu:
+            print("ENABLE_GPU")
+            config.enable_use_gpu(100, device_id)
 
-     if use_mkldnn: 
-       config.enable_mkldnn()
-     self.predictor = create_paddle_predictor(config)
-     
-   def run(self, img_list):
+        if use_mkldnn:
+            config.enable_mkldnn()
+        self.predictor = create_predictor(config)
 
-     input_names = self.predictor.get_input_names()
-     for i, name in enumerate(input_names):
-       input_tensor = self.predictor.get_input_tensor(input_names[i])
-       input_tensor.reshape(img_list[i].shape)   
-       input_tensor.copy_from_cpu(img_list[i].copy())
+    def run(self, img_list):
 
-     self.predictor.zero_copy_run()
+        input_names = self.predictor.get_input_names()
+        for i, name in enumerate(input_names):
+            input_tensor = self.predictor.get_input_handle(input_names[i])
+            input_tensor.reshape(img_list[i].shape)
+            input_tensor.copy_from_cpu(img_list[i].copy())
 
-     results = []
-     output_names = self.predictor.get_output_names()
+        self.predictor.run()
 
-     for i, name in enumerate(output_names):
-       output_tensor = self.predictor.get_output_tensor(output_names[i])
-       output_data = output_tensor.copy_to_cpu()
-       results.append(output_data)
+        results = []
+        output_names = self.predictor.get_output_names()
 
-     return results
+        for i, name in enumerate(output_names):
+            output_tensor = self.predictor.get_output_handle(output_names[i])
+            output_data = output_tensor.copy_to_cpu()
+            results.append(output_data)
 
+        return results
