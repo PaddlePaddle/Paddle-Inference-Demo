a650f31b8186687701fd219222a9fbef6812def0
lidanqing-intel <danqing.li@intel.com>
2021-06-23 05:23:17 +0200

    update

--- c++/x86_gru_int8/run.sh
+++ c++/x86_gru_int8/run.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
-mkdir -p build
-cd build
-rm -rf *
+rm -rf build && mkdir build && cd build
 
 DEMO_NAME=model_test
 
-WITH_MKL=ON
-WITH_GPU=OFF
-USE_TENSORRT=OFF
+LIB_DIR=/home/li/repo/Paddle/build_c5a6ae4/paddle_inference_install_dir
 
-LIB_DIR=$1
+#LIB_DIR=/home/lidanqing/Paddle-Inference-Demo/c++/x86_gru_int8/paddle_inference
+cmake .. -DPADDLE_LIB=${LIB_DIR} -DDEMO_NAME=${DEMO_NAME} && make -j
 
-cmake .. -DPADDLE_LIB=${LIB_DIR} \
-      -DWITH_MKL=${WITH_MKL} \
-        -DDEMO_NAME=${DEMO_NAME} \
-          -DWITH_GPU=${WITH_GPU} \
-            -DWITH_STATIC_LIB=OFF \
+#MODEL_DIR=/home/lidanqing/Paddle-Inference-Demo/c++/x86_gru_int8/GRU_infer_model
+#DATA_DIR=/home/lidanqing/Paddle-Inference-Demo/c++/x86_gru_int8/test_eval_1w.bin
 
-make -j
-
-MODEL_DIR=$2
+MODEL_DIR=/home/li/repo/Paddle-Inference-Demo/c++/x86_gru_int8/GRU_eval_model_v2
+# MODEL_DIR=/home/li/repo/Paddle-Inference-Demo/c++/x86_gru_int8/GRU_eval_int8
+DATA_DIR=/home/li/repo/Paddle-Inference-Demo/c++/x86_gru_int8/test_eval_1w.bin
 default_num_threads=1
-default_with_accuracy=false
+default_with_accuracy=true
 num_threads=${3:-$default_num_threads}
-# with_accuracy_layer=${4:-$default_with_accuracy}
+with_accuracy_layer=${4:-$default_with_accuracy}
 
+#cgdb --args ./${DEMO_NAME} \
 ./${DEMO_NAME} \
-    --model_dir=${MODEL_DIR} \
-    --threads=${num_threads} \
+    --infer_model=${MODEL_DIR} \
+    --infer_data=${DATA_DIR} \
+    --num_threads=${num_threads} \
+    --with_accuracy_layer=${with_accuracy_layer}
