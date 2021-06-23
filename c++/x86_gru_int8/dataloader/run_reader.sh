ccdc632d4a7a1492f58d08c081f48ff33cc4d03d
lidanqing-intel <danqing.li@intel.com>
2021-06-24 01:29:52 +0200

    update

--- c++/x86_gru_int8/dataloader/run_reader.sh
+++ c++/x86_gru_int8/dataloader/run_reader.sh
@@ -3,7 +3,7 @@ python2 my_reader.py \
     --word_emb_dim 128 \
     --grnn_hidden_dim 128 \
     --bigru_num 2 \
-    --test_data ./data/test_8000_bk.tsv \
+    --test_data ./data/test.tsv \
     --word_dict_path ./conf/word.dic \
     --label_dict_path ./conf/tag.dic \
     --word_rep_dict_path ./conf/q2b.dic
