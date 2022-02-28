771ba873a844b47a8ddf748a47a854072883abc2
wangye19 <wangye19@baidu.com>
2022-02-28 02:52:16 +0000

    add ce

--- python/run_demo.sh
+++ python/run_demo.sh
@@ -5,7 +5,7 @@ export RED='\033[0;31m' # red color
 export NC='\033[0m' # no color
 
 # tmp support demos
-test_demos=(cuda_linux_demo ELMO resnet50 x86_linux_demo yolov3)
+test_demos=(cuda_linux_demo ELMo resnet50 x86_linux_demo yolov3)
 
 for demo in ${test_demos[@]};
 do
