2e91c518a171d660e788f5844cdee8e91bb091fe
JZZ-NOTE <jzz2446071400@163.com>
2022-05-26 11:09:07 +0000

    python demo update

---
+++ python/mixed/ELMo/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set +x
+set -e
+
+work_path=$(dirname $(readlink -f $0))
+
+# 1. download model
+if [ ! -d elmo ]; then
+    wget https://paddle-inference-dist.bj.bcebos.com/Paddle-Inference-Demo/elmo.tgz
+    tar xzf elmo.tgz 
+fi
+
+# 2. download data
+if [ ! -d elmo_data ]; then
+    wget https://paddle-inference-dist.bj.bcebos.com/inference_demo/python/elmo/elmo_data.tgz
+    tar xzf elmo_data.tgz
+fi
+
+
+# 3. run
+python infer.py
---
+++ python/mixed/mask_detection/models/pd_model.py
@@ -0,0 +1,42 @@
+import numpy as np
+from paddle.inference import Config
+from paddle.inference import create_predictor
+
+
+class Model:
+    def __init__(self,
+                 model_file,
+                 params_file,
+                 use_mkldnn=True,
+                 use_gpu=False,
+                 device_id=0):
+        config = Config(model_file, params_file)
+        config.enable_memory_optim()
+
+        if use_gpu:
+            print("ENABLE_GPU")
+            config.enable_use_gpu(100, device_id)
+
+        if use_mkldnn:
+            config.enable_mkldnn()
+        self.predictor = create_predictor(config)
+
+    def run(self, img_list):
+
+        input_names = self.predictor.get_input_names()
+        for i, name in enumerate(input_names):
+            input_tensor = self.predictor.get_input_handle(input_names[i])
+            input_tensor.reshape(img_list[i].shape)
+            input_tensor.copy_from_cpu(img_list[i].copy())
+
+        self.predictor.run()
+
+        results = []
+        output_names = self.predictor.get_output_names()
+
+        for i, name in enumerate(output_names):
+            output_tensor = self.predictor.get_output_handle(output_names[i])
+            output_data = output_tensor.copy_to_cpu()
+            results.append(output_data)
+
+        return results
