b478648cb3d3fd714816196bdf5459055e24e6be
zhangjiaming03 <zhangjiaming03@baidu.com>
2022-07-07 17:36:43 +0800

    设置文档主题选项，让目录折叠展示

--- docs-official/conf.py
+++ docs-official/conf.py
@@ -77,6 +77,10 @@ pygments_style = None
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_theme_options = {
+    'collapse_navigation': False,
+}
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
