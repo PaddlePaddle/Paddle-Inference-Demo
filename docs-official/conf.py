d30c028b3301d9f72d7e10e30a7a00f17033bfb5
Shaun2016 <zhangjiaming03@baidu.com>
2022-07-12 19:11:32 +0800

    主题模板layout.html中添加侧边导航 (#325)
    * 文档主题模板layout.html中添加侧边导航
    
    * 文档主题模板layout.html中添加侧边导航

--- docs-official/conf.py
+++ docs-official/conf.py
@@ -79,6 +79,7 @@ html_theme = 'sphinx_rtd_theme'
 
 html_theme_options = {
     'collapse_navigation': False,
+    'titles_only': True
 }
 
 # Theme options are theme-specific and customize the look and feel of a theme
@@ -90,13 +91,13 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # enable word_warp in code
-        ],
-     }
+# html_context = {
+#     'css_files': [
+#         '_static/theme_overrides.css',  # enable word_warp in code
+#         ],
+#      }
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
